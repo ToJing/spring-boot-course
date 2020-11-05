02753b27b3c3841570954f18217f3a695d18f219
Barry <barry@thecover.co>
2020-11-05 16:48:02 +0800

    init

---
+++ server/src/main/java/com/to/jing/course/server/AppServer.java
@@ -0,0 +1,11 @@
+package com.to.jing.course.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppServer {
+    public static void main(String[] args) {
+        SpringApplication.run(AppServer.class);
+    }
+}
